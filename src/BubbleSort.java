dfa302dbac88827853eb6fbbf5b968e4f04c1a1f
ds8611 <ds8611@CSELAB01-28.bennett.edu.in>
2019-02-01 09:23:44 +0530

    Bubble sort

---
+++ src/BubbleSort.java
@@ -0,0 +1,24 @@
+
+public class BubbleSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr1[]= {7,9,10,3,2,1};
+		int temp;
+		for(int j=0; j<arr1.length-1; j++) {
+			for(int i=0; i<arr1.length-1; i++) {
+				if (arr1[i]>arr1[i+1]) {
+					temp=arr1[i+1];
+					arr1[i+1]=arr1[i];
+					arr1[i]=temp;
+				}
+			}
+		}
+		for(int k=0;k<arr1.length;k++) {
+			System.out.println(arr1[k]);
+		}
+		
+
+	}
+
+}
