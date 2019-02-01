a8be3748c3de3ded3d9e0428c553cba4486d4538
ds8611 <ds8611@CSELAB01-28.bennett.edu.in>
2019-02-01 09:42:01 +0530

    merge branch commit for sorting

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -3,7 +3,7 @@ public class BubbleSort {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int arr1[]= {7,9,10,3,2,1,12,5,0};
+		int arr1[]= {7,9,10,3,2,1,12,5,0,77};
 		int temp;
 		for(int j=0; j<arr1.length-1; j++) {
 			for(int i=0; i<arr1.length-1; i++) {
